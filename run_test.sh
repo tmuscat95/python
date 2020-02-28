f232217415848631d4f870f5d5580de4104bb299
tmuscat95 <tim.muscat.95@gmail.com>
2020-02-28 16:35:10 +0100

    Update run_test.sh

--- run_test.sh
+++ run_test.sh
@@ -8,3 +8,34 @@ chmod +x ./install_cybersift.sh
 if [[ $? -ne 0 ]]; then
 	echo "Script Failed With Error: $?"
 fi
+
+ss -a > ss.out
+
+ELSTC=`cat ss.out | grep :9200 | grep java`
+KBNA=`cat ss.out | grep :5601 | grep java`
+NIFI=`cat ss.out | grep :9900 | grep java`
+WEBUI=`cat ss.out | grep "127.0.0.1:80"`
+
+if [[ -z $ELSTC ]]; then
+	echo "Elasticsearch not running on port 9200"
+else
+	echo "Elasticsearch running on port 9200"
+fi
+
+if [[ -z $KBNA ]]; then
+	echo "Kibana not running on port 5601"
+else
+	echo "Kibana running on port 5601"
+fi
+
+if [[ -z $NIFI ]]; then
+	echo "Apache NiFi not running on port 9900"
+else
+	echo "Apache NiFi running on port 9900"
+fi
+
+if [[ -z $WEBUI ]]; then
+	echo "Web UI not running on port 80"
+else 
+	echo "Web UI running on port 80"
+fi
