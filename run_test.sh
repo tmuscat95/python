d6e613cf5a5eccb416cce503e86810c8bc6584f1
tmuscat95 <tim.muscat.95@gmail.com>
2020-02-28 16:10:10 +0100

    Update run_test.sh

--- run_test.sh
+++ run_test.sh
@@ -1,9 +1,10 @@
 cd /root
-curl -H 'Authorization: token 931d1f989c2aa0be087fd6fe655ab970a51d0539' -H 'Accept: application/vnd.github.v3.raw' -O -L https://raw.githubusercontent.com/dvas0004/CS-Install-Scripts/master/install_cybersift.sh > ./install_cybersift.sh
 
+#curl -H 'Authorization: token 931d1f989c2aa0be087fd6fe655ab970a51d0539' -H 'Accept: application/vnd.github.v3.raw' -O -L https://api.github.com/repos/dvas0004/CS-Install-Scripts/contents/install_cybersift.sh > ./install_cybersift.sh
+wget https://csinstallscripts.blob.core.windows.net/install-scripts/install_cybersift.sh
 chmod +x ./install_cybersift.sh
 ./install_cybersift.sh
 
 if [[ $? -ne 0 ]]; then
-	echo "Script failed with $?"
+	echo "Script Failed With Error: $?"
 fi
